29bb59586f8c3ab4237a39764ca2cb7a9bb445eb
Dave Mayo <dave_mayo@harvard.edu>
2015-05-31 18:59:59 -0400

    Basic filtering

--- data/tabs-list.js
+++ data/tabs-list.js
@@ -1,7 +1,33 @@
 "use strict";
 
-self.port.on("show", (tab_data)=> {
+self.port.on("show", (tab_data) => {
   document.getElementById("tabslist").innerHTML = "<li>" +
     [for (td of tab_data) td.title + " (" + td.url + ")"].join("</li><li>") +
     "</li>";
+});
+
+document.getElementById("filter").addEventListener("keyup", (e) => {
+  e.stopPropagation();
+  e.preventDefault();
+
+  let val = e.target.value.replace(/\\/, "\\\\");
+  let els = document.getElementById("tabslist").childNodes;
+  let re;
+  try {
+    re = RegExp(val);
+  }
+  catch (e) {
+    re = null;
+  }
+  Array.prototype.map.bind(els)( (el, i) => {
+    if (!val || !re) {
+      el.className = '';
+    }
+    else if (el.textContent.match(re)) {
+      el.className = "highlight";
+    }
+    else {
+      el.className = '';
+    }
+  });
 });
