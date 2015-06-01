9c426344a3ca1a44131ff9722fe3689bdf250b31
Dave Mayo <dave_mayo@harvard.edu>
2015-05-31 21:40:38 -0400

    Replaced panel with sidebar.

--- data/tabs-list.js
+++ data/tabs-list.js
@@ -1,33 +1,34 @@
 "use strict";
 
-self.port.on("show", (tab_data) => {
+addon.port.on("show", function (tab_data) {
   document.getElementById("tabslist").innerHTML = "<li>" +
     [for (td of tab_data) td.title + " (" + td.url + ")"].join("</li><li>") +
     "</li>";
-});
 
-document.getElementById("filter").addEventListener("keyup", (e) => {
-  e.stopPropagation();
-  e.preventDefault();
+  document.getElementById("filter").addEventListener("keyup", (e) => {
+    e.stopPropagation();
+    e.preventDefault();
 
-  let val = e.target.value.replace(/\\/, "\\\\");
-  let els = document.getElementById("tabslist").childNodes;
-  let re;
-  try {
-    re = RegExp(val);
-  }
-  catch (e) {
-    re = null;
-  }
-  Array.prototype.map.bind(els)( (el, i) => {
-    if (!val || !re) {
-      el.className = '';
+    var val = e.target.value.replace(/\\/, "\\\\"),
+        els = document.getElementById("tabslist").childNodes,
+        re;
+
+    try {
+      re = RegExp(val);
     }
-    else if (el.textContent.match(re)) {
-      el.className = "highlight";
+    catch (e) {
+      re = null;
     }
-    else {
-      el.className = '';
-    }
+    Array.prototype.map.bind(els)( (el, i) => {
+      if (!val || !re) {
+        el.className = '';
+      }
+      else if (el.textContent.match(re)) {
+        el.className = "highlight";
+      }
+      else {
+        el.className = '';
+      }
+    });
   });
 });
